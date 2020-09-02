9df3dd27c3d8134f08691ec4b7e114dbcc0671b2
cyrionpow <qadrrdrmz@gmail.com>
2020-09-02 17:37:28 +0300

    kata has not completed

---
+++ codewarsKatas/find_the_odd_int.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+
+def find_it(seq):
+    max=seq[0]
+    set_num=list(set(seq))
+    for num in set_num:
+        if num in seq:  
+
